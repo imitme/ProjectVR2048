2be9129974e8776c1c4a7b6d9a51de4e2be944d0
imitme_Alienware <7kyeongmin@gmail.com>
2019-02-14 16:27:48 +0900

    1. 컨트롤러 / 총쏘기 : 2. 총구 앞 불꽃 (MuzzleFlashEffect)

--- Assets/VR2048/02.Scripts/FirePosControl.cs
+++ Assets/VR2048/02.Scripts/FirePosControl.cs
@@ -5,20 +5,20 @@ using HTC.UnityPlugin.Vive;
 
 public class FirePosControl : MonoBehaviour
 {
-	public float fireRate = 0.5f;
+	public float fireRate = 0.1f;
 	public GameObject bullet;
+	public ParticleSystem cartridge;
+	public ParticleSystem muzzleFlash;
 
 	public HandRole hand = HandRole.RightHand;
 	public ControllerButton button = ControllerButton.Trigger;
 
 	private float nextFire = 0.0f;
 	private Transform firePos = null;
-	private ParticleSystem _cartridge;
 
 	private void Awake()
 	{
 		firePos=transform;
-		_cartridge=GetComponentInChildren<ParticleSystem>();
 	}
 
 	private void Update()
@@ -39,6 +39,8 @@ public class FirePosControl : MonoBehaviour
 	{
 		Debug.Log("------------------------------------Fire");
 		Instantiate(bullet, firePos.position, firePos.rotation);
-		_cartridge.Play();
+
+		cartridge.Play();
+		muzzleFlash.Play();
 	}
 }
