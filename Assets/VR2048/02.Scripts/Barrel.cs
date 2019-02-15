bbb8ba8d3d185c6786c2d9538e717f1aa820ab83
imitme_Alienware <7kyeongmin@gmail.com>
2019-02-15 10:30:10 +0900

    드럼통 / 총돌 시 모양 바꾸기(meshFilter.sharedMesh = mesh)
    CollBarrel();

--- Assets/VR2048/02.Scripts/Barrel.cs
+++ Assets/VR2048/02.Scripts/Barrel.cs
@@ -4,62 +4,73 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
-	[SerializeField] private string collisionTagNameforExplosion = "BULLET";
+    [SerializeField] private string collisionTagNameforExplosion = "BULLET";
 
-	public GameObject expEffect;
-	public int limitHitCount = 3;
-	[SerializeField] private float expTimeMin = 0.0f;
-	[SerializeField] private float expTimeMax = 0.0f;
+    public GameObject expEffect;
+    public int limitHitCount = 3;
+    [SerializeField] private float expTimeMin = 0.0f;
+    [SerializeField] private float expTimeMax = 0.0f;
+    public Mesh[] meshes;
 
-	private int hitCount = 0;
-	private Rigidbody rb;
-	private float expTime = 0;
-	private GameObject barrel = null;
+    private int hitCount = 0;
+    private Rigidbody rb;
+    private float expTime = 0;
+    private GameObject barrel = null;
+    private MeshFilter meshFilter;
 
-	private void Start()
-	{
-		barrel=this.gameObject;
-		rb=GetComponent<Rigidbody>();
+    private void Start()
+    {
+        barrel = this.gameObject;
+        rb = GetComponent<Rigidbody>();
 
-		expTimeMin=3.0f;
-		expTimeMax=expTimeMin*2.0f;
-		expTime=Random.Range(expTimeMin, expTimeMax);
-	}
+        expTimeMin = 3.0f;
+        expTimeMax = expTimeMin * 2.0f;
+        expTime = Random.Range(expTimeMin, expTimeMax);
 
-	private void OnCollisionEnter(Collision coll)
-	{
-		if(coll.collider.CompareTag(collisionTagNameforExplosion))
-		{
-			if(++hitCount==limitHitCount) { StartCoroutine(ExpBarrelProcess()); }
-		}
-	}
+        meshFilter = GetComponent<MeshFilter>();
+    }
 
-	private IEnumerator ExpBarrelProcess()
-	{
-		MoveBarrel();
-		yield return new WaitForSeconds(expTime);
-		HideBarrelEffects();
-		ShowExpEffect();
-		Destroy(barrel, 2.0f);
-	}
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag(collisionTagNameforExplosion))
+        {
+            CollBarrel();
+            if (++hitCount == limitHitCount) { StartCoroutine(ExpBarrelProcess()); }
+        }
+    }
 
-	private void MoveBarrel()
-	{
-		rb.isKinematic=false;
-	}
+    private void CollBarrel()
+    {
+        int idx = Random.Range(0, meshes.Length);
+        meshFilter.sharedMesh = meshes[idx];
+    }
 
-	private void ShowExpEffect()
-	{
-		Instantiate(expEffect, transform.position, Quaternion.identity);
-	}
+    private IEnumerator ExpBarrelProcess()
+    {
+        MoveBarrel();
+        yield return new WaitForSeconds(expTime);
+        HideBarrelEffects();
+        ShowExpEffect();
+        Destroy(barrel, 2.0f);
+    }
 
-	private void HideBarrelEffects()
-	{
-		Transform[] bulletImpacts = GetComponentsInChildren<Transform>();
-		foreach(var billetImpact in bulletImpacts)
-		{
-			billetImpact.gameObject.SetActive(false);
-		}
-		barrel.GetComponent<MeshRenderer>().enabled=false;
-	}
+    private void MoveBarrel()
+    {
+        rb.isKinematic = false;
+    }
+
+    private void ShowExpEffect()
+    {
+        Instantiate(expEffect, transform.position, Quaternion.identity);
+    }
+
+    private void HideBarrelEffects()
+    {
+        Transform[] bulletImpacts = GetComponentsInChildren<Transform>();
+        foreach (var billetImpact in bulletImpacts)
+        {
+            billetImpact.gameObject.SetActive(false);
+        }
+        barrel.GetComponent<MeshRenderer>().enabled = false;
+    }
 }
