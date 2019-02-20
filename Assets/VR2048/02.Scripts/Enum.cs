e0196ceb706b3672a9ec7457703f6ce24ce09f7d
imitme_Alienware <7kyeongmin@gmail.com>
2019-02-20 12:46:51 +0900

    Menu 만들기 & 버튼인식 Test : for 게임 재시작버튼들 : cf. X버튼에 문제있어 ㅜㅜ

--- Assets/VR2048/02.Scripts/Enum.cs
+++ Assets/VR2048/02.Scripts/Enum.cs
@@ -3,11 +3,6 @@
 	RIFLE = 0, SHOTGUN, FLAMETHROWER, ROCKET
 }
 
-public enum CONTROLLER
-{
-	RIGHT = 0, LEFT, COUNT
-}
-
 public enum EXPTYPE
 {
 	EXP1 = 0, EXP2
--- Assets/VR2048/02.Scripts/Managers/UIManager.cs
+++ Assets/VR2048/02.Scripts/Managers/UIManager.cs
@@ -10,28 +10,35 @@ public class UIManager : MonoBehaviour
 
 	public MoveEvent moveEvent;
 
-	public void OnR_Button()
-	{
+	public void OnR_Button() {
 		moveEvent?.Invoke(DIRECTION.RIGHT);
 	}
 
-	public void OnL_Button()
-	{
+	public void OnL_Button() {
 		moveEvent?.Invoke(DIRECTION.LEFT);
 	}
 
-	public void OnU_Button()
-	{
+	public void OnU_Button() {
 		moveEvent?.Invoke(DIRECTION.UP);
 	}
 
-	public void OnD_Button()
-	{
+	public void OnD_Button() {
 		moveEvent?.Invoke(DIRECTION.DOWN);
 	}
 
-	public void OnNot_Button()
-	{
+	public void OnNot_Button() {
 		moveEvent?.Invoke(DIRECTION.COUNT);
+	}
+
+	public void OnContinue_Button() {
+		Debug.Log("OnContinue_Button");
+	}
+
+	public void OnNewGame_Button() {
+		Debug.Log("OnNewGame_Button");
+	}
+
+	public void OnQuit_Button() {
+		Debug.Log("OnQuit_Button");
 	}
 }
