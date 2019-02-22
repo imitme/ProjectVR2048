d47ed229e969c3ee47ebf5c5fb0c2d9c4c3ba0b4
imitme_Alienware <7kyeongmin@gmail.com>
2019-02-22 21:25:33 +0900

    DestroyByContact / OnTriggerEnter : 무기와 운석 충돌시 정보 매니저에 넘기기

---
+++ Assets/VR2048/02.Scripts/Asteroid/Asteroid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+	public string asteroidName = "Asteroid1";
+	public float chargeGauge = 3.0f;
+	public AsteroidInfo asteroid = null;
+
+	private void Start() {
+		asteroid = gameObject.AddComponent<AsteroidInfo>();
+		asteroid.asteroidName = asteroidName;
+		asteroid.chargeGauge = chargeGauge;
+	}
+}
